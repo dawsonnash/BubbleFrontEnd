003583d12c9cd0a827f35fed4250d8d0a3f0fe57
dawsonnash <dawsonak85@gmail.com>
2023-09-28 15:22:21 -0800

    new welcome page

---
+++ app/src/main/java/com/example/bubblefrontend/WelcomePage.kt
@@ -0,0 +1,81 @@
+package com.example.bubblefrontend
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalContext
+import android.content.Intent
+import com.example.bubblefrontend.ui.theme.BubbleFrontEndTheme
+
+class WelcomePage : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BubbleFrontEndTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WelcomeScreen()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WelcomeScreen() {
+
+    val context = LocalContext.current              // For transitioning to other activities
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Button(
+            onClick = {
+                val intent = Intent(context, RegistrationPage::class.java) // Need to go to Registration Screen
+                context.startActivity(intent)
+            }
+        ) {
+            Text("Register")
+        }
+        Button(
+            onClick = {
+                val intent = Intent(context, ProfilePage::class.java) // Need to go to login screen
+                context.startActivity(intent)
+            }
+        ) {
+            Text("Login")
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomePagePreview() {
+    BubbleFrontEndTheme {
+        WelcomeScreen()
+    }
+}
+
+
