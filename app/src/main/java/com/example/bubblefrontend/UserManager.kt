128628cea49e9f7e27eb54859fb97081c178feb9
dawsonnash <dawsonak85@gmail.com>
2023-09-30 18:08:01 -0800

    Comments updated, again

--- app/src/main/java/com/example/bubblefrontend/UserManager.kt
+++ app/src/main/java/com/example/bubblefrontend/UserManager.kt
@@ -1,26 +1,25 @@
 package com.example.bubblefrontend
 
-// Internal data structure for managing all user information
-// To be replaced upon communication with mySQL database
-// Think about functions we will need make for retrieving data from the database
+// This file is our Internal Data Structure for managing all user information.
+// Should be replaced upon communication with mySQL database
+// Think about functions we will need to make for retrieving data from the database
 // i.e., login validation, user/friend search
 object UserManager {
 
     // Initialize empty HashMap to store username and User object
     private val userMap = HashMap<String, User>()
-    // Data class to hold user information
+
+    // Data class to hold user information, the user object.
     data class User(val username: String, val password: String, val firstName: String, val lastName: String, val email: String) {
     }
 
     init {
-        // Fake user for testing
+        // Fake users for testing
         userMap["bubbleadmin"] = User(username = "bubbleAdmin", password = "123", firstName = "Bit", lastName = "Benders", email = "bitbenders@bubble.com" )
         userMap["dawsonnash"] = User(username = "dawsonnash", password = "123", firstName = "Dawson", lastName = "Nash", email = "dawsonnash@bubble.com" )
         userMap["issacvinson"] = User(username = "issacvinson", password = "123", firstName = "Issac", lastName = "Vinson", email = "issac@bubble.com" )
         userMap["benbrown"] = User(username = "benbrown", password = "123", firstName = "Ben", lastName = "Brown", email = "benbrown@bubble.com" )
         userMap["kaiiiverson"] = User(username = "kaiiiverson", password = "123", firstName = "Kai", lastName = "Iverson", email = "kaiiiverson@bubble.com" )
-
-
     }
 
     // Add a user to the HashMap
@@ -45,7 +44,7 @@ object UserManager {
         return user?.password == password
     }
 
-    // Temporary functions to display registered accounts
+    // Temporary functions to display registered accounts for User Search feature
     fun getUserDetails(username: String): User? {
         return userMap[username]
     }
