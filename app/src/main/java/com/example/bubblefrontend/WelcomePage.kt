4069843f75b86ae9a7e2faae4569161ed88189df
dawsonnash <dawsonak85@gmail.com>
2023-10-03 10:49:56 -0800

    comments

--- app/src/main/java/com/example/bubblefrontend/WelcomePage.kt
+++ app/src/main/java/com/example/bubblefrontend/WelcomePage.kt
@@ -18,6 +18,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalContext
 import android.content.Intent
+import androidx.compose.foundation.Image
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import com.example.bubblefrontend.ui.theme.BubbleFrontEndTheme
 
 class WelcomePage : ComponentActivity() {
@@ -30,7 +33,9 @@ class WelcomePage : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    WelcomeScreen()
+                    Column {
+                        WelcomeScreen()
+                    }
                 }
             }
         }
