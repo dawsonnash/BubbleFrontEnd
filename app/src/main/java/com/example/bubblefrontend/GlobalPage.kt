0774ca3e20e05f198a458815b95ab22d9f8e00f5
IssacVinson <vinsonis03@gmail.com>
2023-10-12 00:25:36 -0800

    Still overlapping posts

--- app/src/main/java/com/example/bubblefrontend/GlobalPage.kt
+++ app/src/main/java/com/example/bubblefrontend/GlobalPage.kt
@@ -1,6 +1,5 @@
 package com.example.bubblefrontend
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.bubblefrontend.ui.theme.BubbleFrontEndTheme
 import androidx.compose.material.icons.Icons
@@ -23,7 +21,8 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.graphics.graphicsLayer
-
+import androidx.compose.ui.platform.LocalContext
+import android.content.Intent
 
 @OptIn(ExperimentalMaterial3Api::class)
 class GlobalPage : ComponentActivity() {
@@ -37,14 +36,57 @@ class GlobalPage : ComponentActivity() {
     }
 }
 
+data class Post(val text: String, var x: Float, var y: Float, var isFull: Boolean = false)
+
+fun positionNewPost(newPost: Post, existingPosts: MutableList<Post>) {
+    val possibleDirections = listOf(
+        Pair(0f, -150f), Pair(0f, 150f), Pair(-150f, 0f), Pair(150f, 0f)
+    )
+
+    var attempts = 0
+    var placed = false
+    var basePost: Post
+
+    while (!placed) {
+        val availablePosts = existingPosts.filter { !it.isFull }
+        if (availablePosts.isNotEmpty()) {
+            basePost = availablePosts.random()
+            val direction = possibleDirections.random()
+            newPost.x = basePost.x + direction.first
+            newPost.y = basePost.y + direction.second
+
+            if (existingPosts.none { isOverlapping(it, newPost) }) {
+                placed = true
+            } else {
+                attempts++
+                if (attempts >= 100) {
+                    basePost.isFull = true
+                    attempts = 0
+                }
+            }
+        } else {
+            newPost.x = 0f
+            newPost.y = 0f
+            placed = true
+        }
+    }
+}
+
+fun isOverlapping(post1: Post, post2: Post): Boolean {
+    val horizontalOverlap = Math.abs(post1.x - post2.x) < 150
+    val verticalOverlap = Math.abs(post1.y - post2.y) < 150
+    return horizontalOverlap && verticalOverlap
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GlobalScreen() {
     var postText by remember { mutableStateOf("") }
     var isPostMenuVisible by remember { mutableStateOf(false) }
-    var posts by remember { mutableStateOf(listOf<String>()) }
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableStateOf(0f) }
+
+    var posts by remember { mutableStateOf(mutableListOf<Post>()) }
 
     Box(
         modifier = Modifier
@@ -56,7 +98,6 @@ fun GlobalScreen() {
                 }
             }
     ) {
-        // For the posts that move with panning
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -65,27 +106,26 @@ fun GlobalScreen() {
                     translationY = offsetY
                 )
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp)
-            ) {
-                // Display all the posts
-                posts.reversed().forEach { post ->
-                    Text(text = post, modifier = Modifier.align(Alignment.CenterHorizontally))
+            posts.forEach { post ->
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .offset(x = post.x.dp, y = post.y.dp)
+                        .background(Color.LightGray, shape = RoundedCornerShape(20.dp))
+                        .padding(16.dp)
+                ) {
+                    Text(text = post.text)
                 }
             }
         }
 
-        // Fixed position UI elements
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            Spacer(modifier = Modifier.weight(1f))  // Pushes the items below to the bottom
+            Spacer(modifier = Modifier.weight(1f))
 
-            // Post creation menu (visible when isPostMenuVisible is true)
             if (isPostMenuVisible) {
                 OutlinedTextField(
                     value = postText,
@@ -94,20 +134,21 @@ fun GlobalScreen() {
                     modifier = Modifier.fillMaxWidth()
                 )
                 Button(onClick = {
-                    posts = posts + postText  // Add the new post to the list
-                    postText = ""  // Clear the input field
-                    isPostMenuVisible = false  // Hide the post creation menu
+                    val newPost = Post(postText, 0f, 0f)
+                    positionNewPost(newPost, posts)
+                    posts.add(newPost)
+                    postText = ""
+                    isPostMenuVisible = false
                 }) {
                     Text("Post")
                 }
             }
 
-            // Plus button to toggle post creation menu
             Button(onClick = { isPostMenuVisible = !isPostMenuVisible }) {
                 Text("+")
             }
 
-            Spacer(modifier = Modifier.height(16.dp))  // Space between the plus button and dashboard
+            Spacer(modifier = Modifier.height(16.dp))
             BottomDashboard()
         }
     }
@@ -116,9 +157,8 @@ fun GlobalScreen() {
 
 @Composable
 fun BottomDashboard() {
-    val context = LocalContext.current  // Obtain the current context
+    val context = LocalContext.current
 
-    // Bottom bar shape and background
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -138,7 +178,6 @@ fun BottomDashboard() {
                     context.startActivity(intent)
                 }
             )
-
             DashboardButton(
                 title = "Search",
                 icon = Icons.Default.Search,
@@ -147,7 +186,6 @@ fun BottomDashboard() {
                     context.startActivity(intent)
                 }
             )
-
             DashboardButton(
                 title = "Profile",
                 icon = Icons.Default.AccountCircle,
@@ -165,8 +203,7 @@ fun DashboardButton(title: String, icon: androidx.compose.ui.graphics.vector.Ima
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .clickable(onClick = onClick)
+        modifier = Modifier.clickable(onClick = onClick)
     ) {
         Icon(imageVector = icon, contentDescription = title)
         Text(text = title)
