c75b8d11b91b27a09449f150dc1e74c9accb097f
IssacVinson <vinsonis03@gmail.com>
2023-10-28 15:31:39 -0800

    Coordinates

--- app/src/main/java/com/example/bubblefrontend/GlobalPage.kt
+++ app/src/main/java/com/example/bubblefrontend/GlobalPage.kt
@@ -1,5 +1,9 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.bubblefrontend
 
+
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,24 +11,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.unit.dp
-import com.example.bubblefrontend.ui.theme.BubbleFrontEndTheme
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
-import android.content.Intent
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.bubblefrontend.ui.theme.BubbleFrontEndTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 class GlobalPage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,52 +39,13 @@ class GlobalPage : ComponentActivity() {
     }
 }
 
-data class Post(val text: String, var x: Float, var y: Float, var isFull: Boolean = false)
-
-fun positionNewPost(newPost: Post, existingPosts: MutableList<Post>) {
-    val possibleDirections = listOf(
-        Pair(0f, -150f), Pair(0f, 150f), Pair(-150f, 0f), Pair(150f, 0f)
-    )
-
-    var attempts = 0
-    var placed = false
-    var basePost: Post
-
-    while (!placed) {
-        val availablePosts = existingPosts.filter { !it.isFull }
-        if (availablePosts.isNotEmpty()) {
-            basePost = availablePosts.random()
-            val direction = possibleDirections.random()
-            newPost.x = basePost.x + direction.first
-            newPost.y = basePost.y + direction.second
-
-            if (existingPosts.none { isOverlapping(it, newPost) }) {
-                placed = true
-            } else {
-                attempts++
-                if (attempts >= 100) {
-                    basePost.isFull = true
-                    attempts = 0
-                }
-            }
-        } else {
-            newPost.x = 0f
-            newPost.y = 0f
-            placed = true
-        }
-    }
-}
-
-fun isOverlapping(post1: Post, post2: Post): Boolean {
-    val horizontalOverlap = Math.abs(post1.x - post2.x) < 150
-    val verticalOverlap = Math.abs(post1.y - post2.y) < 150
-    return horizontalOverlap && verticalOverlap
-}
+data class Post(val text: String, var x: Float, var y: Float)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GlobalScreen() {
     var postText by remember { mutableStateOf("") }
+    var gridPosition by remember { mutableStateOf("") }
     var isPostMenuVisible by remember { mutableStateOf(false) }
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableStateOf(0f) }
@@ -107,14 +71,19 @@ fun GlobalScreen() {
                 )
         ) {
             posts.forEach { post ->
-                Box(
-                    contentAlignment = Alignment.Center,
+                Button(
+                    onClick = { /* Do something when post is clicked */ },
                     modifier = Modifier
                         .offset(x = post.x.dp, y = post.y.dp)
-                        .background(Color.LightGray, shape = RoundedCornerShape(20.dp))
-                        .padding(16.dp)
+                        .size(300.dp)
+                        .background(MaterialTheme.colorScheme.primary, shape = CircleShape)
+                        .graphicsLayer(
+                            shadowElevation = 10.dp.value,
+                            shape = CircleShape,
+                            clip = true
+                        )
                 ) {
-                    Text(text = post.text)
+                    Text(text = post.text, textAlign = androidx.compose.ui.text.style.TextAlign.Center, fontSize = 12.sp)
                 }
             }
         }
@@ -133,11 +102,24 @@ fun GlobalScreen() {
                     label = { Text("Write your post") },
                     modifier = Modifier.fillMaxWidth()
                 )
+                OutlinedTextField(
+                    value = gridPosition,
+                    onValueChange = { gridPosition = it },
+                    label = { Text("Grid Position (X,Y)") },
+                    modifier = Modifier.fillMaxWidth()
+                )
                 Button(onClick = {
-                    val newPost = Post(postText, 0f, 0f)
-                    positionNewPost(newPost, posts)
-                    posts.add(newPost)
-                    postText = ""
+                    try {
+                        val (xStr, yStr) = gridPosition.split(",").map { it.trim() }
+                        val x = xStr.toFloat() * 300  // Example multiplier for positioning
+                        val y = yStr.toFloat() * 300
+                        val newPost = Post(postText, x, y)
+                        posts.add(newPost)
+                        postText = ""
+                        gridPosition = ""
+                    } catch (e: Exception) {
+                        // Handle invalid input
+                    }
                     isPostMenuVisible = false
                 }) {
                     Text("Post")
@@ -154,7 +136,6 @@ fun GlobalScreen() {
     }
 }
 
-
 @Composable
 fun BottomDashboard() {
     val context = LocalContext.current
@@ -163,7 +144,7 @@ fun BottomDashboard() {
         modifier = Modifier
             .fillMaxWidth()
             .height(56.dp)
-            .background(Color.White, shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp))
+            .background(Color.White, shape = androidx.compose.foundation.shape.RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp))
     ) {
         Row(
             modifier = Modifier.fillMaxSize(),
@@ -182,6 +163,7 @@ fun BottomDashboard() {
                 title = "Search",
                 icon = Icons.Default.Search,
                 onClick = {
+                    // Update with the correct class name for UserSearchPage
                     val intent = Intent(context, UserSearchPage::class.java)
                     context.startActivity(intent)
                 }
@@ -190,6 +172,7 @@ fun BottomDashboard() {
                 title = "Profile",
                 icon = Icons.Default.AccountCircle,
                 onClick = {
+                    // Update with the correct class name for ProfilePage
                     val intent = Intent(context, ProfilePage::class.java)
                     context.startActivity(intent)
                 }
