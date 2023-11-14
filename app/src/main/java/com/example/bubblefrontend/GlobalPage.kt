d4bcab53207db6e2e7675b55e8b8fa2b32197113
IssacVinson <vinsonis03@gmail.com>
2023-11-13 21:08:26 -0900

    Hex grid with borders showing 625 post which are all buttons that exand the post to the whole page

--- app/src/main/java/com/example/bubblefrontend/GlobalPage.kt
+++ app/src/main/java/com/example/bubblefrontend/GlobalPage.kt
@@ -1,8 +1,6 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.example.bubblefrontend
 
-
+import kotlin.math.sqrt
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -21,11 +19,20 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import com.example.bubblefrontend.ui.theme.BubbleFrontEndTheme
 
 class GlobalPage : ComponentActivity() {
@@ -39,102 +46,130 @@ class GlobalPage : ComponentActivity() {
     }
 }
 
-data class Post(val text: String, var x: Float, var y: Float)
 
-@OptIn(ExperimentalMaterial3Api::class)
+data class Post(
+    val text: String?,
+    val pictureUrl: String? // Assuming pictures are represented by URLs
+    //Example: Post(text = "Some text", pictureUrl = "http://example.com/picture.jpg")
+    //Example: Post(text = null, pictureUrl = "http://example.com/picture.jpg")
+)
+
+fun initializePosts(): List<Post> {
+    val posts = mutableListOf<Post>()
+    for (i in 1..625) {
+        posts.add(Post(text = "Initialization Post $i", pictureUrl = null)) // Adding a number for differentiation
+    }
+    return posts
+}
+
+@Composable
+fun FullScreenPostView(post: Post, onBack: () -> Unit) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column {
+            Button(onClick = onBack) {
+                Text("Back")
+            }
+
+            Text(
+                text = post.text ?: "",
+                fontSize = 20.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth().padding(16.dp)
+            )
+
+            // Only display the image if there is a picture URL
+            post.pictureUrl?.let {
+                Image(
+                    painter = painterResource(id = R.drawable.profile_icon), // Replace with your actual image resource
+                    contentDescription = "Post Image",
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
+        }
+    }
+}
+
+
 @Composable
 fun GlobalScreen() {
-    var postText by remember { mutableStateOf("") }
-    var gridPosition by remember { mutableStateOf("") }
-    var isPostMenuVisible by remember { mutableStateOf(false) }
-    var offsetX by remember { mutableStateOf(0f) }
-    var offsetY by remember { mutableStateOf(0f) }
+    val posts = remember { mutableStateListOf(*initializePosts().toTypedArray()) }
+    var selectedPost by remember { mutableStateOf<Post?>(null) }
+    var showDialog by remember { mutableStateOf(false) }
 
-    var posts by remember { mutableStateOf(mutableListOf<Post>()) }
+    if (selectedPost != null) {
+        // Render the full screen view for the selected post
+        FullScreenPostView(post = selectedPost!!, onBack = { selectedPost = null })
+    } else {
+        // Render the grid of posts
+        val configuration = LocalConfiguration.current
+        val density = LocalDensity.current
+        val bubbleSizePx = with(density) { 400.dp.toPx() }
+        val bubbleRadiusPx = bubbleSizePx / 1.25f
+        val verticalDistancePx = bubbleRadiusPx * sqrt(3f) * 3 / 4
+        val horizontalDistancePx = bubbleRadiusPx * 2 * 0.75f
+        val columns = 25
+        val totalGridWidthPx = columns * horizontalDistancePx
+        val screenWidthPx = with(density) { configuration.screenWidthDp.dp.toPx() }
+        val screenHeightPx = with(density) { configuration.screenHeightDp.dp.toPx() }
+        val maxHorizontalScrollPx = totalGridWidthPx - (1.2f * screenWidthPx) + (horizontalDistancePx / 2)
+        val maxVerticalScrollPx = (totalGridWidthPx - (2 * screenHeightPx) + (horizontalDistancePx / 2)) / 1.5f  //2.37 for 2500 posts  2.00 for 625 posts
+        val initialOffsetX = -maxHorizontalScrollPx / 2
+        val initialOffsetY = -maxVerticalScrollPx / 2
+        var offsetX by remember { mutableStateOf(initialOffsetX) }
+        var offsetY by remember { mutableStateOf(initialOffsetY) }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .pointerInput(Unit) {
-                detectTransformGestures { _, pan, _, _ ->
-                    offsetX += pan.x
-                    offsetY += pan.y
-                }
-            }
-    ) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
+                .pointerInput(Unit) {
+                    detectTransformGestures { _, pan, _, _ ->
+                        val newOffsetX = (offsetX + pan.x).coerceIn(-maxHorizontalScrollPx, 0f)
+                        val newOffsetY = (offsetY + pan.y).coerceIn(-maxVerticalScrollPx, 0f)
+                        offsetX = newOffsetX
+                        offsetY = newOffsetY
+                    }
+                }
                 .graphicsLayer(
                     translationX = offsetX,
                     translationY = offsetY
                 )
         ) {
-            posts.forEach { post ->
-                Button(
-                    onClick = { /* Do something when post is clicked */ },
+            posts.forEachIndexed { index, post ->
+                val col = index % columns
+                val row = index / columns
+                val xOffset = if (row % 2 == 0) col * horizontalDistancePx else col * horizontalDistancePx + horizontalDistancePx / 2
+                val yOffset = row * verticalDistancePx * 3 / 4
+
+                Box(
                     modifier = Modifier
-                        .offset(x = post.x.dp, y = post.y.dp)
-                        .size(300.dp)
-                        .background(MaterialTheme.colorScheme.primary, shape = CircleShape)
-                        .graphicsLayer(
-                            shadowElevation = 10.dp.value,
-                            shape = CircleShape,
-                            clip = true
+                        .offset(x = with(density) { xOffset.toDp() }, y = with(density) { yOffset.toDp() })
+                        .size(400.dp)
+                        .background(
+                            brush = Brush.radialGradient(
+                                colors = listOf(Color.Magenta, Color.Cyan, Color.Yellow, Color.Magenta),
+                                center = Offset.Zero,
+                                radius = bubbleRadiusPx
+                            ),
+                            shape = CircleShape
                         )
+                        .padding(16.dp)
+                        .clickable {
+                            selectedPost = post // Update the state to the clicked post
+                        },
+                    contentAlignment = Alignment.Center
                 ) {
-                    Text(text = post.text, textAlign = androidx.compose.ui.text.style.TextAlign.Center, fontSize = 12.sp)
+                    Text(
+                        text = post.text ?: "",
+                        fontSize = 16.sp,
+                        color = Color.White
+                    )
                 }
             }
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-        ) {
-            Spacer(modifier = Modifier.weight(1f))
-
-            if (isPostMenuVisible) {
-                OutlinedTextField(
-                    value = postText,
-                    onValueChange = { postText = it },
-                    label = { Text("Write your post") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                OutlinedTextField(
-                    value = gridPosition,
-                    onValueChange = { gridPosition = it },
-                    label = { Text("Grid Position (X,Y)") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                Button(onClick = {
-                    try {
-                        val (xStr, yStr) = gridPosition.split(",").map { it.trim() }
-                        val x = xStr.toFloat() * 300  // Example multiplier for positioning
-                        val y = yStr.toFloat() * 300
-                        val newPost = Post(postText, x, y)
-                        posts.add(newPost)
-                        postText = ""
-                        gridPosition = ""
-                    } catch (e: Exception) {
-                        // Handle invalid input
-                    }
-                    isPostMenuVisible = false
-                }) {
-                    Text("Post")
-                }
-            }
-
-            Button(onClick = { isPostMenuVisible = !isPostMenuVisible }) {
-                Text("+")
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
-            BottomDashboard()
         }
     }
 }
+
 
 @Composable
 fun BottomDashboard() {
